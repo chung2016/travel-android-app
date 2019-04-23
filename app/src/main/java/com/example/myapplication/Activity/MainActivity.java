455be4c7f4ef034fe9bff38a6b124d760aa5522d
Cheng Chi Chung <187109370@stu.vtc.edu.hk>
2019-04-24 00:10:37 +0800

    add activity model

--- app/src/main/java/com/example/myapplication/Activity/MainActivity.java
+++ app/src/main/java/com/example/myapplication/Activity/MainActivity.java
@@ -1,8 +1,10 @@
-package com.example.myapplication;
+package com.example.myapplication.Activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.ImageView;
+
+import com.example.myapplication.R;
 
 public class MainActivity extends AppCompatActivity {
 
